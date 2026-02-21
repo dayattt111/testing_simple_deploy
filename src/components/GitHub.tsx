d50f37a414f703e8937f5ced88895b0dbc17d0c0
Muhammad Amin Hidayat <tumbaldaya@gmail.com>
2026-02-21 22:50:57 +0800

    update: component github

--- src/components/GitHub.tsx
+++ src/components/GitHub.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './GitHub.css'
 
-const GITHUB_USERNAME = 'saidsudirman'
+const GITHUB_USERNAME = 'barce860'
 
 interface GitHubUser {
   login: string
